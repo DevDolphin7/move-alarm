0e7294409c566a4dc832d900ff5307df71a9750b
DevDolphin7 <devdolphin7@outlook.com>
2025-02-13 21:10:53 +0000

    Get token for UA test

--- src/app.py
+++ src/app.py
@@ -1,2 +1,12 @@
-def say_hello(name):
-    return f"Hello {name}!"
+from utils.utils import HandleAuthorisation
+
+
+def get_token():
+    ha = HandleAuthorisation()
+
+    token = ha.get_token()
+
+    print(token)
+
+
+get_token()
