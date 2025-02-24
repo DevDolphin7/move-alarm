89afd4a839d77f3a2506b6a4df4cbbede64eb4ec
DevDolphin7 <devdolphin7@outlook.com>
2025-02-24 16:58:20 +0000

    Config fully tested

--- __tests__/test_config.py
+++ __tests__/test_config.py
@@ -1,1 +1,282 @@
+import os, configparser, datetime
+import pytest, pytest_mock
 from move_alarm.utils.config import Configuration
+import move_alarm.types as datatype
+
+
+class TestConfig:
+
+    @property
+    def config_path(self) -> str:
+        try:
+            return self._config_path
+        except:
+            self._config_path = os.path.join(
+                os.path.dirname(__file__)[:-9], "move_alarm", "test_config.ini"
+            )
+            return self._config_path
+
+    @pytest.fixture(autouse=True)
+    def before_each(self):
+        if os.path.exists(self.config_path):
+            os.remove(self.config_path)
+
+    @pytest.fixture
+    def create_mock_config_file(self):
+        config_path = self.config_path
+
+        def _create_mock_config_file(data):
+            parser = configparser.ConfigParser()
+            parser.read_dict(data)
+
+            with open(config_path, "w") as file:
+                parser.write(file)
+
+        return _create_mock_config_file
+
+    @pytest.fixture(name="Create good mock config file")
+    def create_mock_good_config_file(self, create_mock_config_file):
+        good_data = {
+            "Alarm": datatype.IniFormattedAlarm(
+                interval=3600,
+                snooze=300,
+                message="Time to move!",
+            ),
+            "Sounds": datatype.IniFormattedSounds(
+                path=os.path.dirname(__file__),
+                freesound=False,
+                themes=["piano", "guitar"],
+            ),
+        }
+
+        create_mock_config_file(good_data)
+
+    @pytest.fixture(name="Create missing key mock config file")
+    def create_mock_missing_key_config_file(self, create_mock_config_file):
+        tampered_data = {
+            "Alarm": datatype.IniFormattedAlarm(
+                interval=3600,
+                snooze=300,
+                message="Time to move!",
+            ),
+            "Sounds": datatype.IniFormattedSounds(
+                path=os.path.dirname(__file__),
+                freesound=False,
+            ),
+        }
+
+        create_mock_config_file(tampered_data)
+
+    @pytest.fixture(name="Create bad value type mock config file")
+    def create_mock_bad_value_type_config_file(self, create_mock_config_file):
+        tampered_data = {
+            "Alarm": datatype.IniFormattedAlarm(
+                interval=3600,
+                snooze="three hundred",
+                message="Time to move!",
+            ),
+            "Sounds": datatype.IniFormattedSounds(
+                path=os.path.dirname(__file__),
+                freesound=False,
+                themes=["piano"],
+            ),
+        }
+
+        create_mock_config_file(tampered_data)
+
+    @pytest.fixture(name="Prevent set_config_file initiation call creating a new file")
+    def prevent_set_config_file(self, monkeypatch: pytest.MonkeyPatch):
+        monkeypatch.setattr(
+            "move_alarm.utils.config.Configuration.set_config_file",
+            lambda *args, **kwargs: None,
+        )
+
+    class TestInitialisation:
+        @property
+        def config_path(self) -> str:
+            return TestConfig.config_path.fget(self)
+
+        def test_requires_str_config_path_on_initialisation(self):
+            with pytest.raises(TypeError):
+                Configuration()
+
+        def test_raises_type_error_on_non_str_argument(self):
+            with pytest.raises(TypeError):
+                Configuration(True)
+
+        def test_invokes_load_config_file_on_initiation(
+            self, mocker: pytest_mock.MockerFixture
+        ):
+            mock_load_config_file = mocker.patch(
+                "move_alarm.utils.config.Configuration.load_config_file"
+            )
+
+            Configuration(self.config_path)
+
+            mock_load_config_file.assert_called_once()
+
+        @pytest.mark.usefixtures("Create missing key mock config file")
+        def test_warns_user_on_loading_error(self, capfd: pytest.CaptureFixture):
+            Configuration(self.config_path)
+
+            out, err = capfd.readouterr()
+
+            assert (
+                out
+                == "Warning: No option 'themes' in section: 'Sounds'\nUsing default values...\n"
+            )
+
+        @pytest.mark.usefixtures("Create missing key mock config file")
+        @pytest.mark.usefixtures(
+            "Prevent set_config_file initiation call creating a new file"
+        )
+        def test_invokes_use_default_values_on_loading_error(
+            self, mocker: pytest_mock.MockerFixture
+        ):
+
+            mock_use_default_values = mocker.patch(
+                "move_alarm.utils.config.Configuration.use_default_values",
+            )
+
+            Configuration(self.config_path)
+
+            mock_use_default_values.assert_called()
+
+        def test_invokes_set_config_file_on_loading_error(
+            self, mocker: pytest_mock.MockerFixture
+        ):
+            mock_set_config_file = mocker.patch(
+                "move_alarm.utils.config.Configuration.set_config_file"
+            )
+
+            config = Configuration(self.config_path)
+
+            mock_set_config_file.assert_called_once()
+
+    class TestSetConfigFile:
+        @property
+        def config_path(self) -> str:
+            return TestConfig.config_path.fget(self)
+
+        def test_creates_a_config_file_at_config_path(self):
+            assert os.path.exists(self.config_path) == False
+
+            config = Configuration(self.config_path)
+            config.set_config_file()
+
+            assert os.path.exists(self.config_path) == True
+
+        def test_config_file_contains_only_valid_properties(self):
+            config = Configuration(self.config_path)
+
+            config.set_config_file()
+
+            output = configparser.ConfigParser()
+            output.read(self.config_path)
+
+            wait_duration = datetime.timedelta(
+                seconds=output.getint("Alarm", "interval")
+            )
+            snooze_duration = datetime.timedelta(
+                seconds=output.getint("Alarm", "snooze")
+            )
+            reminder_text = output.get("Alarm", "message")
+            wav_directory = output.get("Sounds", "path")
+            api_enabled = output.getboolean("Sounds", "freesound")
+            sound_themes = list(output.get("Sounds", "themes"))
+
+            for var in [wait_duration, snooze_duration]:
+                assert isinstance(var, datetime.timedelta) == True
+
+            for var in [reminder_text, wav_directory]:
+                assert isinstance(var, str) == True
+
+            assert isinstance(api_enabled, bool) == True
+            assert isinstance(sound_themes, list) == True
+
+            assert len(sound_themes) > 0
+            for theme in sound_themes:
+                assert isinstance(theme, str) == True
+
+        def test_returns_true_on_success(self):
+            config = Configuration(self.config_path)
+
+            output = config.set_config_file()
+
+            assert output == True
+
+    class TestLoadConfig:
+
+        @property
+        def config_path(self) -> str:
+            return TestConfig.config_path.fget(self)
+
+        @pytest.mark.usefixtures("Create good mock config file")
+        def test_sets_all_valid_variables_to_valid_data_types(self):
+
+            config = Configuration(self.config_path)
+
+            config.load_config_file()
+
+            assert isinstance(config.wait_duration, datetime.timedelta) == True
+            assert isinstance(config.snooze_duration, datetime.timedelta) == True
+            assert isinstance(config.reminder_text, str) == True
+            assert isinstance(config.wav_directory, str) == True
+            assert isinstance(config.api_enabled, bool) == True
+            assert isinstance(config.sound_themes, list) == True
+
+        @pytest.mark.usefixtures("Create good mock config file")
+        def test_returns_true_on_success(self):
+            config = Configuration(self.config_path)
+
+            output = config.load_config_file()
+
+            assert output == True
+
+        @pytest.mark.usefixtures(
+            "Prevent set_config_file initiation call creating a new file"
+        )
+        def test_raises_file_not_found_on_missing_file(self):
+            config = Configuration(self.config_path)
+
+            with pytest.raises(FileNotFoundError):
+                config.load_config_file()
+
+        @pytest.mark.usefixtures("Create missing key mock config file")
+        @pytest.mark.usefixtures(
+            "Prevent set_config_file initiation call creating a new file"
+        )
+        def test_raises_value_error_on_missing_key(self):
+            config = Configuration(self.config_path)
+
+            with pytest.raises(configparser.NoOptionError):
+                config.load_config_file()
+
+        @pytest.mark.usefixtures("Create bad value type mock config file")
+        @pytest.mark.usefixtures(
+            "Prevent set_config_file initiation call creating a new file"
+        )
+        def test_raises_type_error_on_incorrect_value_types(self):
+            config = Configuration(self.config_path)
+
+            with pytest.raises(ValueError):
+                config.load_config_file()
+
+    class TestUseDefaultValues:
+        @property
+        def config_path(self) -> str:
+            return TestConfig.config_path.fget(self)
+
+        def test_sets_appropriate_default_values(self):
+            config = Configuration(self.config_path)
+
+            config.use_default_values()
+
+            assert config.wait_duration == datetime.timedelta(hours=1)
+            assert config.snooze_duration == datetime.timedelta(minutes=5)
+            assert config.reminder_text == "Time to stretch!"
+            assert config.wav_directory == os.path.join(
+                os.path.dirname(__file__)[:-9], "move_alarm", "assets"
+            )
+            assert config.api_enabled == False
+            assert config.sound_themes == ["funk"]
--- move_alarm/utils/config.py
+++ move_alarm/utils/config.py
@@ -1,7 +1,149 @@
-import os
+import configparser, datetime, os
+import move_alarm.types as datatype
 
 
 class Configuration:
-    wav_directory: str = os.path.join(os.path.dirname(__file__)[:-5], "assets")
-    api_enabled: bool = False
-    sound_themes: list[str] = []
+
+    @property
+    def config_path(self) -> str:
+        return self.__config_path
+
+    @config_path.setter
+    def config_path(self, path: str) -> None:
+        if isinstance(path, str):
+            self.__config_path = path
+        else:
+            raise TypeError("Configuration must be initialised with a string.")
+
+    @property
+    def wait_duration(self) -> datetime.timedelta:
+        return self.__wait_duration
+
+    @wait_duration.setter
+    def wait_duration(self, duration: datetime.timedelta) -> None:
+        if isinstance(duration, datetime.timedelta):
+            self.__wait_duration = duration
+        else:
+            raise TypeError("datetime.timedelta required for wait_duration")
+
+    @property
+    def snooze_duration(self) -> datetime.timedelta:
+        return self.__snooze_duration
+
+    @snooze_duration.setter
+    def snooze_duration(self, duration: datetime.timedelta) -> None:
+        if isinstance(duration, datetime.timedelta):
+            self.__snooze_duration = duration
+        else:
+            raise TypeError("datetime.timedelta required for snooze_duration")
+
+    @property
+    def reminder_text(self) -> str:
+        return self.__reminder_text
+
+    @reminder_text.setter
+    def reminder_text(self, message: str) -> None:
+        if isinstance(message, str):
+            self.__reminder_text = message
+        else:
+            raise TypeError("str required for reminder_text")
+
+    @property
+    def wav_directory(self) -> str:
+        return self.__wav_directory
+
+    @wav_directory.setter
+    def wav_directory(self, dir: str) -> None:
+        if isinstance(dir, str) and os.path.exists(dir):
+            self.__wav_directory = dir
+        else:
+            raise ValueError(
+                "A string representing an existing directory required for wav_directory"
+            )
+
+    @property
+    def api_enabled(self) -> bool:
+        return self.__api_enabled
+
+    @api_enabled.setter
+    def api_enabled(self, value: bool) -> None:
+        if isinstance(value, bool):
+            self.__api_enabled = value
+        else:
+            raise TypeError("bool required for api_enabled")
+
+    @property
+    def sound_themes(self) -> list[str]:
+        return self.__sound_themes
+
+    @sound_themes.setter
+    def sound_themes(self, themes: list[str]) -> None:
+        if isinstance(themes, list):
+            self.__sound_themes = [theme for theme in themes if isinstance(theme, str)]
+        else:
+            raise TypeError("list[str] required for sound_themes")
+
+    def __init__(self, config_path: str):
+        self.config_path = config_path
+
+        try:
+            self.load_config_file()
+        except Exception as error:
+            print(f"Warning: {Warning(error)}\nUsing default values...")
+            self.use_default_values()
+            self.set_config_file()
+
+    def use_default_values(self) -> None:
+        self.wait_duration = datetime.timedelta(minutes=60)
+        self.snooze_duration = datetime.timedelta(minutes=5)
+        self.reminder_text = "Time to stretch!"
+        self.wav_directory = os.path.abspath(
+            os.path.join(os.path.dirname(__file__)[:-5], "assets")
+        )
+        self.api_enabled = False
+        self.sound_themes = ["funk"]
+
+    def define_data_to_save(self) -> datatype.IniFormattedConfig:
+        return {
+            "Alarm": datatype.IniFormattedAlarm(
+                interval=int(self.wait_duration.total_seconds()),
+                snooze=int(self.snooze_duration.total_seconds()),
+                message=self.reminder_text,
+            ),
+            "Sounds": datatype.IniFormattedSounds(
+                path=self.wav_directory,
+                freesound=self.api_enabled,
+                themes=self.sound_themes,
+            ),
+        }
+
+    def set_config_file(self) -> bool:
+        data = self.define_data_to_save()
+
+        parser = configparser.ConfigParser()
+        parser.read_dict(data)
+
+        with open(self.config_path, "w") as file:
+            parser.write(file)
+
+        return True
+
+    def load_config_file(self) -> bool:
+        if not os.path.exists(self.config_path):
+            raise FileNotFoundError(self.config_path)
+
+        config_parser = configparser.ConfigParser()
+        config_parser.read(self.config_path)
+
+        self.wait_duration = datetime.timedelta(
+            seconds=config_parser.getint("Alarm", "interval")
+        )
+        self.snooze_duration = datetime.timedelta(
+            seconds=config_parser.getint("Alarm", "snooze")
+        )
+        self.reminder_text = config_parser.get("Alarm", "message")
+        self.wav_directory = config_parser.get("Sounds", "path")
+        self.api_enabled = config_parser.getboolean("Sounds", "freesound")
+        self.sound_themes = list(config_parser.get("Sounds", "themes"))
+
+        return True
